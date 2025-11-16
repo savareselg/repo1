ea69ad9bafc42591ce016b6c734cbed51912483d
savareselg <savareselg@gmail.com>
2025-11-16 19:44:23 +0100

    Primo script bash

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Definisci i percorsi delle cartelle
+CARTELLA_A="/path/to/cartella_A"
+CARTELLA_B="/path/to/cartella_B"
+
+# Controlla se la cartella A esiste
+if [ ! -d "$CARTELLA_A" ]; then
+    echo "Errore: La cartella A non esiste: $CARTELLA_A"
+    exit 1
+fi
+
+# Crea la cartella B se non esiste
+mkdir -p "$CARTELLA_B"
+
+# Copia tutti i file dalla cartella A alla cartella B
+cp -v "$CARTELLA_A"/* "$CARTELLA_B"
+
+echo "Copia completata!"
